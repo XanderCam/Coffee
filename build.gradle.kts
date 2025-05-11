d8aa0f20b8de107ce201e8afea26ee4b718c9b74
CodeSandbox <support@codesandbox.io>
2025-05-11 16:41:58 +0000

    Fix deprecated buildDir usage in clean task

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)
 }
